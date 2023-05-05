933c7bcc42a6ee097d1b315efbdb67b19e04e41b
RubelBiswasDH <RubelBiswasDH@gmail.com>
2023-05-05 16:32:13 +0600

    Modified index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ export default function Home() {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Let's get started with........&nbsp;
           <code className="font-mono font-bold">Docker and Jenkins</code>
+          <span>{ 'Updated' }</span>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
