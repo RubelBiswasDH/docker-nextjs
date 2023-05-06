284f832d7a2a1217f447c35d261430a2d25d8318
RubelBiswasDH <RubelBiswasDH@gmail.com>
2023-05-06 11:25:54 +0600

    Added google analytics

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,5 @@
 export const NEXT_PUBLIC_KEY = process.env.NEXT_PUBLIC_KEY || 'no-keys'
-
+export const KEYS = {
+    GOOGLE_ANALYTICS_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ''
+}
 console.log('key: ', NEXT_PUBLIC_KEY)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
+import { KEYS } from '@/app.config'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <Script strategy="afterInteractive" src={ `https://www.googletagmanager.com/gtag/js?id=${ KEYS.GOOGLE_ANALYTICS_ID }` } />
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', KEYS.GOOGLE_ANALYTICS_ID, {
+            page_path: window.location.pathname,
+          });
+        `,
+      }}
+    />
+    <Component {...pageProps} />
+  </>)
 }
