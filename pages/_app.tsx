6c6ce583310c6a146e56d14527d9406ebfdf4821
RubelBiswasDH <RubelBiswasDH@gmail.com>
2023-05-09 00:58:23 +0600

    Modified Keys

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,9 +14,11 @@ export default function App({ Component, pageProps }: AppProps) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', KEYS.GOOGLE_ANALYTICS_ID, {
-            page_path: window.location.pathname,
-          });
+          try{
+            gtag('config', KEYS.GOOGLE_ANALYTICS_ID, {
+              page_path: window.location.pathname,
+            });
+          } catch (e) { console.log('') }
         `,
       }}
     />
