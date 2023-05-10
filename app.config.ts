0bdf48a9dd362490ef852d0a52be0b4fe2749477
RubelBiswasDH <RubelBiswasDH@gmail.com>
2023-05-10 11:16:37 +0600

    Added New Env Variable

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,5 @@
 export const NEXT_PUBLIC_KEY = process.env.NEXT_PUBLIC_KEY || 'no-keys'
+export const NEXT_PUBLIC_VAR = process.env.NEXT_PUBLIC_VAR || 'NEXT_PUBLIC_VAR not found'
 export const KEYS = {
     GOOGLE_ANALYTICS_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ''
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { NEXT_PUBLIC_KEY } from '@/app.config'
+import { NEXT_PUBLIC_KEY, NEXT_PUBLIC_VAR } from '@/app.config'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className='font-mono font-bold text-orange-800'>{ `Next Var: ${ NEXT_PUBLIC_VAR }` }</p>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           { `Let's get started with........May 11, 12:26 AM` }
